6fdfff47836ce24881f680483c6cff1b1cbfea34
ajensen25 <aidenjensen707@gmail.com>
2025-03-26 15:43:32 -0400

    finished styling for content page

--- src/components/App/Content/Content.jsx
+++ src/components/App/Content/Content.jsx
@@ -3,12 +3,18 @@ import "./Content.css";
 function Content() {
   return (
     <div className="content">
+      <h2></h2>
       <h1 className="timer">15</h1>
       <div className="word-list">
         <p>The tiny red fox jumped over the large black fence.</p>
       </div>
+      <div className="typing-display">
+        <p>The tiny red fox</p>
+      </div>
       <div className="typing-stats">
-        <h2>WPM: 112</h2>
+        <h2>
+          WPM: <h2>112</h2>
+        </h2>
         <p>
           Characters per minute: <h2>520</h2>
         </p>
