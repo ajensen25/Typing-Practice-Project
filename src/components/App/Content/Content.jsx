24f50a2a4f8f9b5e20ffcfb43895db2ebae987e8
ajensen25 <aidenjensen707@gmail.com>
2025-03-28 11:26:55 -0400

    changed user text to input box and added event listener

--- src/components/App/Content/Content.jsx
+++ src/components/App/Content/Content.jsx
@@ -1,25 +1,38 @@
+import { useRef, useState } from "react";
 import "./Content.css";
 
 function Content() {
+  const [userSentence, setUserSentence] = useState("");
+  const typingInput = useRef(null);
+
+  const onKeyPressed = (e) => {
+    console.log(e.key);
+  };
+
   return (
     <div className="content">
-      <h2></h2>
+      <p className="title">Begin typing to start the timer.</p>
       <h1 className="timer">15</h1>
       <div className="word-list">
         <p>The tiny red fox jumped over the large black fence.</p>
       </div>
       <div className="typing-display">
-        <p>The tiny red fox</p>
+        <input
+          type="text"
+          placeholder="Begin the test here"
+          ref={typingInput}
+          onKeyUp={onKeyPressed}
+        />
       </div>
       <div className="typing-stats">
-        <h2>
-          WPM: <h2>112</h2>
-        </h2>
         <p>
-          Characters per minute: <h2>520</h2>
+          Characters per minute: <span>520</span>
         </p>
         <p>
-          Completion time: <h2>8.6 seconds</h2>
+          Words per minute: <span>112</span>
+        </p>
+        <p>
+          Completion time: <span>8.6 sec</span>
         </p>
       </div>
     </div>
